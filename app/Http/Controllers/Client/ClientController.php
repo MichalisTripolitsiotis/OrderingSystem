0b697309bb047beebbdf9425303bd6706b532897
Michalis Tripolitsiotis <michalistripolitsiotis@gmail.com>
2021-04-04 19:49:23 +0300

    Make dynamic categories

--- app/Http/Controllers/Client/ClientController.php
+++ app/Http/Controllers/Client/ClientController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -11,10 +12,21 @@ class ClientController extends Controller
 
     public function index()
     {
+        $categories = Category::all();
         $products = Product::all();
-
         return view('client.home', [
             'products' => $products,
+            'categories' => $categories
+        ]);
+    }
+
+    public function show($id)
+    {
+        $categories = Category::all();
+        $products = Category::find($id)->products;
+        return view('client.home', [
+            'products' => $products,
+            'categories' => $categories
         ]);
     }
 }
