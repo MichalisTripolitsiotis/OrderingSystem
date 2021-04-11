ffab584fae5d7d4ed09f870d7f20651402165322
Michalis Tripolitsiotis <michalistripolitsiotis@gmail.com>
2021-04-11 17:10:52 +0300

    Start making the welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,20 @@
-@extends('layouts.app')
-
+@extends('layouts.guest')
+{{-- @extends('layouts.app') --}}
 @section('content')
+  <section id="hero">
+    <div class="hero-container" data-aos="zoom-in" data-aos-delay="100">
+      <h1>Orderator</h1>
+      <h2>Order your pizza and track them in real time</h2>
+      <a href="#about" class="btn-get-started">Get Started</a>
+    </div>
+  </section>
+  <main id="main">
+    <section id="about">
+      <div class="container" data-aos="fade-up">
+        <div class="row main-section">
+        </div>
 
-
+      </div>
+    </section>
+  </main>
 @endsection
